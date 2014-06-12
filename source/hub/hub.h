ba53cc9dbda3986b8be2b99148d1b4e9e5dd1b95
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-06-12 14:33:20 +0900

    Add empty project

---
+++ source/hub/hub.h
@@ -0,0 +1,13 @@
+//
+//  hub.h
+//  hub
+//
+//  Created by Kataoka Naoyuki on 2014/06/12.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hub : NSObject
+
+@end
---
+++ source/hub/hub.m
@@ -0,0 +1,13 @@
+//
+//  hub.m
+//  hub
+//
+//  Created by Kataoka Naoyuki on 2014/06/12.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import "hub.h"
+
+@implementation hub
+
+@end
