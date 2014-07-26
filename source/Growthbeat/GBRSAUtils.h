47f875a914b870287a6ff149220634ecb173ad6d
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-07-27 04:35:02 +0900

    Add GBRSAUtils class to encrypt data

---
+++ source/Growthbeat/GBRSAUtils.h
@@ -0,0 +1,15 @@
+//
+//  GBRSAUtils.h
+//  Growthbeat
+//
+//  Created by Kataoka Naoyuki on 2014/07/25.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBRSAUtils : NSObject
+
++ (NSString *)encrypt:(NSString *)data publicKey:(NSString *)publicKey;
+
+@end
---
+++ source/Growthbeat/GBRSAUtils.m
@@ -0,0 +1,13 @@
+//
+//  GBRSAUtils.m
+//  Growthbeat
+//
+//  Created by Kataoka Naoyuki on 2014/07/25.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import "GBRSAUtils.h"
+
+@implementation GBRSAUtils
+
+@end
